3473731bc8748e78560701603fb5c1f9c4aed648
pcx <pcx@DESKTOP-59BHL0C.mshome.net>
2019-09-22 20:17:50 -0400

    added some actual networking code

--- src/common/PacketHandler.java
+++ src/common/PacketHandler.java
@@ -1,9 +1,43 @@
 package common;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Handler;
+
 public class PacketHandler implements Runnable{
+	
+	
 	
 	@Override
 	public void run() {
+		System.out.println("The chat server is running...");
+        ExecutorService pool = Executors.newFixedThreadPool(500);
+        try (ServerSocket listener = new ServerSocket(59001)) {
+            while (true) {
+                pool.execute(new ClientChannel(listener.accept()));
+            }
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private class ClientChannel implements Runnable
+	{
+		
+		private Socket socket;
+
+		public ClientChannel(Socket sock) {
+			socket = sock;
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
 		
 	}
 
